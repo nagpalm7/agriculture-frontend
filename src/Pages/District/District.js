4b4b5e8a8eb90c0efedc32c591f9a6fb8b4f14a2
sanchi-chugh <sanchichugh2001@gmail.com>
2020-09-01 14:51:51 +0530

    district and villages page addition

--- src/Pages/District/District.js
+++ src/Pages/District/District.js
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { PageHeader, Button, Input, Table, Tag, Space } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
-const District = () => {
-  return <div> District</div>;
-};
+const { Search } = Input;
+
+const columns = [
+  {
+    title: 'District',
+    dataIndex: 'district',
+    key: 'name',
+  },
+  {
+    title: 'Option',
+    key: 'option',
+    render: (text, record) => (
+      <Space size="small">
+        <a>
+          <EditOutlined />
+        </a>
+        <a>
+          <DeleteOutlined />
+        </a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    district: 'village name',
+  },
+  {
+    key: '2',
+    district: 'village name',
+  },
+  {
+    key: '3',
+    district: 'village name',
+  },
+  {
+    key: '4',
+    district: 'village name',
+  },
+  {
+    key: '5',
+    district: 'village name',
+  },
+  {
+    key: '6',
+    district: 'village name',
+  },
+  {
+    key: '7',
+    district: 'village name',
+  },
+  {
+    key: '8',
+    district: 'village name',
+  },
+];
+
+class District extends Component {
+  render() {
+    return (
+      <>
+        <div className="site-page-header-ghost-wrapper">
+          <PageHeader
+            ghost={false}
+            title="District"
+            subTitle=""
+            extra={[
+              <Button key="1" shape="round">
+                Add
+              </Button>,
+              <Button key="2" shape="round">
+                Add Bulk
+              </Button>,
+              <Search
+                placeholder="input search text"
+                onSearch={(value) => console.log(value)}
+                style={{ width: 200 }}
+              />,
+            ]}>
+            <Table columns={columns} dataSource={data} size="small" />
+          </PageHeader>
+        </div>
+      </>
+    );
+  }
+}
 
 export default District;
