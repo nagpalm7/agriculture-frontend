f7a00df7b21fcf62eec78ad3cb97b04ca3454429
sanchi-chugh <sanchichugh2001@gmail.com>
2020-09-02 18:28:45 +0530

    dashboard design improved

--- src/Layouts/AdminDashboard/AdminDashboard.jsx
+++ src/Layouts/AdminDashboard/AdminDashboard.jsx
@@ -51,95 +51,97 @@ class AdminDashboard extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <Layout>
-          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-            <div className="logo">
-              {this.state.collapsed ? 'AFL' : 'AFL Monitoring'}
-            </div>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1" icon={<HomeFilled />}>
-                <Link to="/">Home</Link>
-              </Menu.Item>
-              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                <Link to="/district"> District</Link>
-              </Menu.Item>
-              <Menu.Item key="3" icon={<UploadOutlined />}>
-                <Link to="/villages">Villages</Link>
-              </Menu.Item>
-              <Menu.Item key="4" icon={<TeamOutlined />}>
-                <Link to="/dda">DDA</Link>
-              </Menu.Item>
-              <Menu.Item key="5" icon={<UserOutlined />}>
-                <Link to="/ado">ADO</Link>
-              </Menu.Item>
-              <Menu.Item key="6" icon={<UserOutlined />}>
-                <Link to="/locations">
-                  {' '}
-                  Locations
-                  <Dropdown overlay={menu}>
-                    <a
-                      className="ant-dropdown-link"
-                      onClick={(e) => e.preventDefault()}>
-                      <DownOutlined />
-                    </a>
-                  </Dropdown>
-                </Link>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }}>
-              <PageHeader
-                ghost={false}
-                title=""
-                subTitle=""
-                extra={[
-                  <>
-                    <Button shape="round">Analysis</Button>
-                    <Button
-                      shape="round"
-                      htmlType="submit"
-                      onClick={() => this.props.logout()}>
-                      Logout
-                    </Button>
-                  </>,
-                ]}
-              />
-              {React.createElement(
-                this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-                {
-                  className: 'trigger',
-                  onClick: this.toggle,
-                },
-              )}
-            </Header>
-
-            <Content
-              className="site-layout-background"
-              style={{
-                margin: '24px 16px',
-                padding: 24,
-                minHeight: 280,
-              }}>
-              <div style={{ flex: 1, padding: '10px' }}>
-                <Switch>
-                  {dashboard_routes.map((route, index) => (
-                    <Route
-                      key={index}
-                      path={route.path}
-                      exact={route.exact}
-                      children={<route.component />}
-                    />
-                  ))}
-                </Switch>
+      <div style={{ overflow: 'hidden' }}>
+        <BrowserRouter>
+          <Layout>
+            <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+              <div className="logo">
+                {this.state.collapsed ? 'AFL' : 'AFL Monitoring'}
               </div>
-            </Content>
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                <Menu.Item key="1" icon={<HomeFilled />}>
+                  <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                  <Link to="/district">District</Link>
+                </Menu.Item>
+                <Menu.Item key="3" icon={<UploadOutlined />}>
+                  <Link to="/villages">Villages</Link>
+                </Menu.Item>
+                <Menu.Item key="4" icon={<TeamOutlined />}>
+                  <Link to="/dda">DDA</Link>
+                </Menu.Item>
+                <Menu.Item key="5" icon={<UserOutlined />}>
+                  <Link to="/ado">ADO</Link>
+                </Menu.Item>
+                <Menu.Item key="6" icon={<UserOutlined />}>
+                  <Link to="/locations">
+                    {' '}
+                    Locations
+                    <Dropdown overlay={menu}>
+                      <a
+                        className="ant-dropdown-link"
+                        onClick={(e) => e.preventDefault()}>
+                        <DownOutlined />
+                      </a>
+                    </Dropdown>
+                  </Link>
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Layout className="site-layout">
+              <Header className="site-layout-background" style={{ padding: 0 }}>
+                <PageHeader
+                  ghost={false}
+                  title=""
+                  subTitle=""
+                  extra={[
+                    <>
+                      <Button className="headerButton">Analysis</Button>
+                      <Button
+                        className="headerButton"
+                        htmlType="submit"
+                        onClick={() => this.props.logout()}>
+                        Logout
+                      </Button>
+                    </>,
+                  ]}
+                />
+                {React.createElement(
+                  this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                  {
+                    className: 'trigger',
+                    onClick: this.toggle,
+                  },
+                )}
+              </Header>
 
-            <Footer>footer</Footer>
+              <Content
+                className="site-layout-background"
+                style={{
+                  margin: '35px 17px 0px 49px',
+                  padding: 24,
+                  minHeight: 280,
+                }}>
+                <div style={{ flex: 1, padding: '10px' }}>
+                  <Switch>
+                    {dashboard_routes.map((route, index) => (
+                      <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        children={<route.component />}
+                      />
+                    ))}
+                  </Switch>
+                </div>
+              </Content>
+
+              <Footer>footer</Footer>
+            </Layout>
           </Layout>
-        </Layout>
-      </BrowserRouter>
+        </BrowserRouter>
+      </div>
     );
   }
 }
