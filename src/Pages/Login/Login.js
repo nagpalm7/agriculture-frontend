c7bb929ecc073d5576a7f7991bfaab3d9f689b9a
harsh gandhi <harshgandhi043@gmail.com>
2020-09-05 12:15:52 +0530

    made login page responsive

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
-import loginImage from '../../assets/images/loginImage.svg';
+import { Form, Button, Input, Checkbox, Row, Col } from 'antd';
 import './Login.css';
 
 class Login extends Component {
@@ -35,7 +34,8 @@ class Login extends Component {
     // event.preventDefault();
     this.setState({ loadings: true });
     const { username, password } = this.state;
-    axios.post('https://api.aflmonitoring.com/rest-auth/login/', {
+    axios
+      .post('https://api.aflmonitoring.com/rest-auth/login/', {
         username: username,
         password: password,
       })
@@ -66,70 +66,76 @@ class Login extends Component {
   render() {
     const { loadings } = this.state;
     return (
-      <div>
-        <Row>
-          <Col span={8}>
-            <div>
-              <img src={loginImage} alt="login page" className="image" />
-              <div className="top-left">
-                <h1>AFL Monitoring</h1>
-              </div>
-            </div>
-          </Col>
-          <Col span={16}>
-            <Form
-              onValuesChange={this.handleChange}
-              name="normal_login"
-              className="login-form"
-              onFinish={this.handleSubmit}>
-              <h2>Log In</h2>
-              <h5>Username</h5>
-              <Form.Item
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your username!',
-                  },
-                ]}>
-                <Input placeholder="Username" />
+      <div className="main-content">
+        <div className="left-content">
+          <h1 style={{ textAlign: 'center' }}>AFL Monitoring</h1>
+        </div>
+        <div className="right-content">
+          <Form
+            onValuesChange={this.handleChange}
+            name="normal_login"
+            className="login-form"
+            onFinish={this.handleSubmit}>
+            <h1>
+              <b>Log In</b>
+            </h1>
+            <h5>
+              <b>Username</b>
+            </h5>
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+              style={{ marginBottom: '16px' }}>
+              <Input placeholder="Username" />
+            </Form.Item>
+            <h5>
+              <b>Password</b>
+            </h5>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Password!',
+                },
+              ]}
+              style={{ marginBottom: '8px' }}>
+              <Input type="password" placeholder="Password" />
+            </Form.Item>
+            <Form.Item
+              valuePropName="unchecked"
+              style={{ marginBottom: '16px' }}>
+              <Checkbox
+                name="checked"
+                checked={this.state.checked}
+                onChange={this.onCheckboxChange}>
+                Remember me
+              </Checkbox>
+            </Form.Item>
+            <Form.Item>
+              <Form.Item style={{ marginBottom: '16px' }}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  loading={loadings}
+                  style={{ width: '150px' }}>
+                  Log in
+                </Button>
               </Form.Item>
-              <h5>Password</h5>
-              <Form.Item
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your Password!',
-                  },
-                ]}>
-                <Input type="password" placeholder="Password" />
-              </Form.Item>
-              <Form.Item valuePropName="unchecked">
-                <Checkbox
-                  name="checked"
-                  checked={this.state.checked}
-                  onChange={this.onCheckboxChange}>
-                  Remember me
-                </Checkbox>
-              </Form.Item>
-              <Form.Item>
-                <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    loading={loadings}
-                    className="login-form-button">
-                    Log in
-                  </Button>
-                </Form.Item>
-                <a className="login-form-forgot" href="/">
-                  Forgot Password?
-                </a>
-              </Form.Item>
-            </Form>
-          </Col>
-        </Row>
+              <a
+                className="login-form-forgot"
+                href="/"
+                style={{ color: '#3d0098' }}>
+                Forgot Password?
+              </a>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     );
   }
