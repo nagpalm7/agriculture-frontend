7bcdaea2b161cde633c53b59bcd559cd576fd151
harsh gandhi <harshgandhi043@gmail.com>
2020-09-12 01:54:43 +0530

    add village validation and block and ado list dropdown added and made edit form

--- src/Pages/Villages/components/AddVillage.js
+++ src/Pages/Villages/components/AddVillage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Input, Button, Typography, message } from 'antd';
+import { Form, Input, Button, Typography, message, Select } from 'antd';
 import './addvillage.css';
 
 import { axiosInstance } from '../../../utils/axiosIntercepter';
@@ -11,7 +11,50 @@ class AddVillages extends Component {
     super();
     this.state = {
       loadings: false,
+      blockData: [],
+      adoData: [],
     };
+  }
+
+  componentDidMount() {
+    // console.log(this.blockRef.current);
+    axiosInstance
+      .get('/api/block/')
+      .then((res) => {
+        const blockData = res.data.map((item) => {
+          return {
+            block: item.block,
+            id: item.id,
+          };
+        });
+        this.setState({ ...this.state, blockData: blockData });
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response);
+        } else {
+          console.log(err.message);
+        }
+      });
+
+    axiosInstance
+      .get('/api/users-list/ado/')
+      .then((res) => {
+        const adoData = res.data.results.map((item) => {
+          return {
+            ado: item.user.username,
+            id: item.user.id,
+          };
+        });
+        this.setState({ ...this.state, adoData: adoData });
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response);
+        } else {
+          console.log(err.message);
+        }
+      });
   }
 
   handleAddVillage = (event) => {
@@ -52,7 +95,15 @@ class AddVillages extends Component {
           <h3>
             <b>Village</b>
           </h3>
-          <Form.Item name="village_name" style={{ marginBottom: '10px' }}>
+          <Form.Item
+            name="village_name"
+            style={{ marginBottom: '10px' }}
+            rules={[
+              {
+                required: true,
+                message: 'Please provide village name!',
+              },
+            ]}>
             <Input
               placeholder="Village name"
               style={{ borderRadius: '7px', borderColor: '#707070' }}
@@ -61,7 +112,15 @@ class AddVillages extends Component {
           <h3>
             <b>Village Code</b>
           </h3>
-          <Form.Item name="village_code" style={{ marginBottom: '10px' }}>
+          <Form.Item
+            name="village_code"
+            style={{ marginBottom: '10px' }}
+            rules={[
+              {
+                required: true,
+                message: 'Please provide village code!',
+              },
+            ]}>
             <Input
               placeholder="Village Code"
               style={{ borderRadius: '7px', borderColor: '#707070' }}
@@ -70,7 +129,15 @@ class AddVillages extends Component {
           <h3>
             <b>Village Sub Code</b>
           </h3>
-          <Form.Item name="village_subcode" style={{ marginBottom: '10px' }}>
+          <Form.Item
+            name="village_subcode"
+            style={{ marginBottom: '10px' }}
+            rules={[
+              {
+                required: true,
+                message: 'Please provide village subcode!',
+              },
+            ]}>
             <Input
               placeholder="Village Sub Code"
               style={{ borderRadius: '7px', borderColor: '#707070' }}
@@ -79,11 +146,46 @@ class AddVillages extends Component {
           <h3>
             <b>Block</b>
           </h3>
-          <Form.Item name="block" style={{ marginBottom: '16px' }}>
-            <Input
-              placeholder="Block"
-              style={{ borderRadius: '7px', borderColor: '#707070' }}
-            />
+          <Form.Item
+            name="blocklist"
+            style={{ marginBottom: '16px' }}
+            rules={[
+              {
+                required: true,
+                message: 'Please select block!',
+              },
+            ]}>
+            <Select
+              placeholder="Select Block"
+              style={{ borderRadius: '7px', borderColor: '#707070' }}>
+              {this.state.blockData.map((item) => {
+                return (
+                  <Select.Option value={item.id}>{item.block}</Select.Option>
+                );
+              })}
+            </Select>
+          </Form.Item>
+          <h3>
+            <b>Ado</b>
+          </h3>
+          <Form.Item
+            name="adolist"
+            style={{ marginBottom: '16px' }}
+            rules={[
+              {
+                required: true,
+                message: 'Please select ado!',
+              },
+            ]}>
+            <Select
+              placeholder="Select Ado"
+              style={{ borderRadius: '7px', borderColor: '#707070' }}>
+              {this.state.adoData.map((item) => {
+                return (
+                  <Select.Option value={item.id}>{item.ado}</Select.Option>
+                );
+              })}
+            </Select>
           </Form.Item>
           <Form.Item>
             <Form.Item style={{ marginBottom: '10px' }}>
--- src/Pages/Villages/components/EditVillage.js
+++ src/Pages/Villages/components/EditVillage.js
@@ -1,8 +1,100 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
+import { Form, Input, Button, Typography } from 'antd';
+import { axiosInstance } from '../../../utils/axiosIntercepter';
 
+const { Title } = Typography;
 class EditVillage extends Component {
+  constructor() {
+    super();
+    this.formRef = createRef();
+  }
+
+  componentDidMount() {
+    // console.log(this.formRef.current);
+    console.log('mounted');
+    // console.log(this.props.match);
+    // const {
+    //   match: { params },
+    // } = this.props;
+    // axiosInstance
+    //   .get(`/api/village/${params.villageId}`)
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     if (err.response) {
+    //       console.log(err.response);
+    //     } else {
+    //       console.log(err.message);
+    //     }
+    //   });
+  }
+
   render() {
-    return <div>Edit Village component</div>;
+    return (
+      <div className="edit-village-container">
+        <div>
+          <Title level={3}>Edit Village</Title>
+        </div>
+        <Form
+          ref={this.formRef}
+          name="edit_village"
+          className="edit-village"
+          onFinish={this.handleEditVillage}>
+          <h3>
+            <b>Village</b>
+          </h3>
+          <Form.Item name="village_name" style={{ marginBottom: '10px' }}>
+            <Input
+              placeholder="Village name"
+              style={{ borderRadius: '7px', borderColor: '#707070' }}
+            />
+          </Form.Item>
+          <h3>
+            <b>Village Code</b>
+          </h3>
+          <Form.Item name="village_code" style={{ marginBottom: '10px' }}>
+            <Input
+              placeholder="Village Code"
+              style={{ borderRadius: '7px', borderColor: '#707070' }}
+            />
+          </Form.Item>
+          <h3>
+            <b>Village Sub Code</b>
+          </h3>
+          <Form.Item name="village_subcode" style={{ marginBottom: '10px' }}>
+            <Input
+              placeholder="Village Sub Code"
+              style={{ borderRadius: '7px', borderColor: '#707070' }}
+            />
+          </Form.Item>
+          <h3>
+            <b>Block</b>
+          </h3>
+          <Form.Item name="block" style={{ marginBottom: '16px' }}>
+            <Input
+              placeholder="Block"
+              style={{ borderRadius: '7px', borderColor: '#707070' }}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item style={{ marginBottom: '10px' }}>
+              <Button
+                htmlType="submit"
+                className="edit-village-btn"
+                style={{
+                  background: '#3d0098',
+                  borderColor: '#3d0098',
+                  color: '#ffffff',
+                  fontWeight: '500',
+                }}>
+                Update
+              </Button>
+            </Form.Item>
+          </Form.Item>
+        </Form>
+      </div>
+    );
   }
 }
 
