5c1fb1f3ddd57ce506925e02ab35804c0f75d5db
Yuvraj <yuvraj1mann@gmail.com>
2021-04-22 01:56:51 +0530

    home_tab_new_changes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { Row, Col, Spin, message, Select,Button } from 'antd';
+import { Row, Col, Spin, message, Select, Button, Divider } from 'antd';
 import './Home.css';
 import Map from './Map';
 import Charts from './Charts';
 import { axiosInstance } from '../../utils/axiosIntercepter';
 import DropdownMenu from './Dropdown';
-
+import { Tooltip } from 'antd';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       locations: null,
-      renderLocations:null,
+      renderLocations: null,
       districts: null,
       selectedDist: null,
       loading: true,
-      times:1,
+      times: 1,
     };
   }
   fetchData = async () => {
@@ -30,12 +30,12 @@ class Home extends Component {
       let dists = await axiosInstance.get(
         'https://api.aflmonitoring.com/api/district/',
       );
-      var toRender=locs.data.slice(0,(locs.data.length/4));
-      console.log(locs.data,locs.data.length);
+      var toRender = locs.data.slice(0, locs.data.length / 4);
+      console.log(locs.data, locs.data.length);
       dists.data.push({ id: -1, district: 'ALL DISTRICTS' });
       this.setState({
         locations: locs.data,
-        renderLocations:toRender,
+        renderLocations: toRender,
         districts: dists.data,
         selectedDist: 'ALL DISTRICTS',
         loading: false,
@@ -96,41 +96,62 @@ class Home extends Component {
             }}
           />
         </Row>
-        <Row><Button disabled={!(this.state.times<4)} onClick={()=>{
-          var n=this.state.locations.length;
-          var newLocs;
-          var times=this.state.times;
-          if(times==1){
-            newLocs=this.state.locations.slice(n/4+1,n/2);
-          }
-          if(times==2){
-            newLocs=this.state.locations.slice(n/2+1,(3*n)/4);
-          }
-          if(times==3){
-            newLocs=this.state.locations.slice((3*n)/4,n);
-          }
-          var upLocs=[...this.state.renderLocations,...newLocs];
-          this.setState(
-            {
-            ...this.state,
-            renderLocations:upLocs,
-            times:this.state.times+1,
-            }
-          )
-          console.log(upLocs);
-          }}>Add More</Button></Row>
+        <Divider></Divider>
+        <Row className="add_more_locs">
+          <Tooltip
+            title={`Add locations.Page might go unresponsive until new locations are rendered.`}>
+            <Button
+              disabled={!(this.state.times < 4)}
+              style={{
+                backgroundColor: '#e03b3b',
+                color: 'white',
+                marginBottom: '5px',
+                borderRadius: '10px',
+              }}
+              onClick={() => {
+                var n = this.state.locations.length;
+                var newLocs;
+                var times = this.state.times;
+                if (times == 1) {
+                  newLocs = this.state.locations.slice(n / 4 + 1, n / 2);
+                }
+                if (times == 2) {
+                  newLocs = this.state.locations.slice(n / 2 + 1, (3 * n) / 4);
+                }
+                if (times == 3) {
+                  newLocs = this.state.locations.slice((3 * n) / 4, n);
+                }
+                var upLocs = [...this.state.renderLocations, ...newLocs];
+                this.setState({
+                  ...this.state,
+                  renderLocations: upLocs,
+                  times: this.state.times + 1,
+                });
+              }}>
+              Add More Locations
+            </Button>
+          </Tooltip>
+
+          <div className="count_displayer">
+            Locations currently displayed -{' '}
+            {this.state.renderLocations
+              ? this.state.renderLocations.length
+              : '0'}
+          </div>
+        </Row>
         <Row justify="center" className="map_wrapper">
           {!this.state.loading ? (
             <>
               <Col lg={18} sm={24} xs={24}>
                 <Map locations={this.state.renderLocations} />
               </Col>
+
               <Col lg={6} sm={24} xs={24}>
                 <Charts selectedDist={this.state.selectedDist} />
               </Col>
             </>
           ) : (
-            <Spin/>
+            <Spin />
           )}
         </Row>
       </div>
